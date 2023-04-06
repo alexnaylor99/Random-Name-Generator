003aa2236faca746ce0ce3a2b1c7887e90a84752
BirchAD <andrewbirch1882@icloud.com>
2023-04-06 14:59:11 +0100

    check for empty strings and remove them

--- main.py
+++ main.py
@@ -12,12 +12,16 @@ def user_inputs():
             print("Error: Name list should only contain letters and commas.")
             continue
         name_list = remove_duplicates(name_list.split(','))
+        name_list = check_for_empty_string(name_list)
+        if not check_name_list_not_empty(name_list):
+            print("Error: List of names cannot be empty.")
+            continue
         num_of_names = get_valid_num_of_names(name_list)
         break
     return name_list, num_of_names
 
 def get_valid_num_of_names(name_list):
-    """Confirms user input for the number is an integer and lower than the number of names in the list submitted"""
+    """Gets and user input for the number is an integer and lower than the number of names in the list submitted"""
     while True:
         num_of_names = input(f"Enter a number lower than {len(name_list)} to randomly select that number of names from the list: ")
         if not num_of_names.isdigit():
@@ -42,6 +46,18 @@ def remove_duplicates(name_list):
 def remove_whitespace_after_commas(name_list):
     """Remove whitespace after commas in the name list."""
     return re.sub(r',\s*', ',', name_list)
+
+def check_name_list_not_empty(name_list):
+    """Check if the name list is not empty."""
+    if not name_list:
+        return False
+    return True
+
+def check_for_empty_string(name_list):
+    """Check for empty strings in a list and remove them."""
+    name_list = [name for name in name_list if name != '']
+    return name_list
+
 
 def generate_random_names():
     """Generates then prints randomly selected names from the list of names submitted by the user"""
