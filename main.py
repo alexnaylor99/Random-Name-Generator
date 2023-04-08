894a6e9dc20933e9516f36aa7fd835fbc67219f7
Tariq Smith <tariqanthonysmith@gmail.com>
2023-04-08 15:51:59 +0100

    Completed with Functions

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
 import random
 
-short_list = input("Please type the names into the list: \n").split(" ")
 
-is_number = False
-number_of_people = ""
+def main():
+    print(random_sample(input_list(), number_people()))
 
-while not is_number:
-    number_of_people = input("How many people would you like to select? \n")
-    if number_of_people.isnumeric():
-        number_of_people = int(number_of_people)
-        is_number = True
-    else:
-        continue
 
-new_list = random.sample(short_list, number_of_people)
-print("Here is your random sample list: \n" + str(new_list))
+def input_list():
+    short_list = input("Please type the names into the list: \n").split(" ")
+    return short_list
+
+
+def number_people():
+    num_people = 0
+    while num_people == 0:
+        people = input("How many people would you like to select? \n")
+        num_people = is_positive_integer(people)
+    return num_people
+
+
+def is_positive_integer(number):
+    if number.isnumeric():
+        if int(number) != 0:
+            number = int(number)
+            return number
+    return 0
+
+
+def random_sample(original_list, num_people):
+    new_list = random.sample(original_list, num_people)
+    return "Here is your random sample list: \n" + str(new_list)
+
+
+main()
 
 
 
