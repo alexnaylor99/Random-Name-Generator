4a6657acf1f5c82559cea35cefa80690b0474538
ctorrington1 <ctorrington1@gmail.com>
2023-04-08 13:19:11 +0100

    method return types

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class PythonBBNamer:
     be unable to choose from as well.
     """
 
-    def is_name_list_correct(self):
+    def is_name_list_correct(self) -> bool:
         """Ensure the names provided are legal for a human entity."""
         
         correct_name_regex = r'[^a-zA-Z]'
@@ -27,7 +27,7 @@ class PythonBBNamer:
             
         return True
 
-    def is_amount_of_names_correct(self):
+    def is_amount_of_names_correct(self) -> bool:
         """Ensure the parent is not already asking for the impossible."""
         
         correct_amount_of_names_flag = [True]
@@ -49,7 +49,7 @@ class PythonBBNamer:
 
         return correct_amount_of_names_flag[0]
 
-    def get_names_list(self,):
+    def get_names_list(self) -> None:
         """Get the names that the parent cannot decide upon"""
         
         self.names_list = input("Enter the names that you want, seperated by a space below\nEg: Chris Christopher ChrisJnr:\n").split(' ')
@@ -59,8 +59,7 @@ class PythonBBNamer:
             # print("\nPlease enter valid identifiers for your child.")
             self.get_names_list()
 
-    # Get the amount of names to be displayed
-    def get_amount_of_names(self):
+    def get_amount_of_names(self) -> None:
         """Get the amount of names to display"""
         
         self.amount_of_names = int(input("\nHow many you names do you want to choose from:\n"))
@@ -70,7 +69,7 @@ class PythonBBNamer:
             # print("Please enter a valid number of names to display.")
             self.get_amount_of_names()
 
-    def get_name_suggestions(self):
+    def get_name_suggestions(self) -> list:
         """Return a random selection of elements from the valid names provided 
         without replacement. The returned selection sized is supplied."""
 
@@ -83,7 +82,7 @@ class PythonBBNamer:
         return name_suggestions
     
 
-    def intro(self):
+    def intro(self) -> None:
         """Introdce the uncreative owner to the program."""
         
         print("So, you can't think of a name for your own child.")
