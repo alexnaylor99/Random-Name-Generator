82338c91951dbfe9708a69d54353f5ddb1bf596f
Ruy Zambrano <Ruyzambrano@gmail.com>
2023-04-21 16:44:05 +0100

    I added the user part now. They add a list and a number to generate n number of names from the list. I also added error handling.

--- main.py
+++ main.py
@@ -3,7 +3,30 @@
 
 import random
 
-def random_name_generator(list_names, num_of_names):
-    shortlist = random.choices(list_names, k=num_of_names)
+def random_name_generator(list_names, num_names):
+    shortlist = random.choices(list_names, k=num_names)
     return shortlist
 
+if __name__ == '__main__':
+    names = input('Enter a list of names that you are choosing from: ')
+    list_names = names.split()
+    while True:
+        try:
+            num_names = int(input('How many names do you want: '))
+            if num_names <= 0:
+                print('You must choose a number greater than 0. Try again')
+                continue
+            break
+        except Exception as e:
+            print(f'Error: {e}\nTry again')
+    shortlist = ''
+    if len(list_names) > num_names:
+        shortlist = random_name_generator(list_names, num_names)
+    elif len(list_names) == num_names:
+        shortlist = list_names
+    else:
+        print('There aren\'t enough names on the list!')
+    if shortlist:
+        print('Here is your shortlist:')
+        for count, name in enumerate(shortlist, 1):
+            print(f'{count}. {name}')
