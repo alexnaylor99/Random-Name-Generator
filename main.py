2ac32b639472e0b298a0e832c4837362965b33c0
HineshX <hinesh.tailor@xandertalent.com>
2023-04-24 20:00:11 +0100

    added a while loop

--- main.py
+++ main.py
@@ -6,16 +6,18 @@ def random_name_generator():
     list_of_names = list_of_names.split(",")
     list_of_names = [i.strip(" ") for i in list_of_names]
 
-    num_of_names = input("Enter the number of names you would like:")
-
-    if num_of_names.isdigit() is False or len(list_of_names) < int(num_of_names) or int(num_of_names) <= 0:
-        print("Please enter an integer which is greater than 0 and less than or equal to the number number of names you have added")
-    else:
-        num_of_names = int(num_of_names)
-        selected_names = random.sample(list_of_names, num_of_names)
-        print("Here are your names:")
-        for name in selected_names:
-            print(name)
+    while True:
+        num_of_names = input("Enter the number of names you would like:")
+        if num_of_names.isdigit() is False or len(list_of_names) < int(num_of_names) or int(num_of_names) <= 0:
+            print("Please enter an integer which is greater than 0 and less than or equal to the number number of names you have added")
+            continue
+        else:
+            num_of_names = int(num_of_names)
+            selected_names = random.sample(list_of_names, num_of_names)
+            break
+    print("Here are your names:")
+    for name in selected_names:
+        print(name)
 
 
 random_name_generator()
