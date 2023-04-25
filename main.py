8ca12a3d68d504b6e385c90453e1646ca29a8882
Ruy Zambrano <Ruyzambrano@gmail.com>
2023-04-25 11:56:58 +0100

    Finished with PEP8

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
+'''The Random-Name-Generator takes in a list of names and a number
+names wanted, then returns a list of random names.
+'''
 import random
-'''
-The `random` module provides functions for generating random
- numbers and making random selections in Python.
-'''
 
 
 def random_name_generator(shortlist_names, int_names):
     '''
-    Takes in a list of names and an amount of names desired, 
+    Takes in a list of names and an amount of names desired,
     then uses the random module to return a list of names.
 
     Parameters
@@ -20,22 +19,22 @@ def random_name_generator(shortlist_names, int_names):
     Function returns a list of names
     '''
     random_names = random.sample(shortlist_names, int_names)
+
     return random_names
 
 
 if __name__ == '__main__':
-
     # Prompt the user to input a list of names and split them into a list.
     while True:
         names = input('Enter a list of names that you are '
                       'choosing from, separated by a space '
                       '(eg: "Anna Elsa Moana"): ')
-        
+
         # Check if the user has input names.
         if names == '':
             print('You must enter at lease 1 name. Try again.')
             continue
-        
+
         list_names = names.split()
         break
 
@@ -51,7 +50,7 @@ if __name__ == '__main__':
         except ValueError as e:
             print(f'Error: {e}.\nYou must enter an integer. Try again')
 
-    # Generate the shortlist. 
+    # Generate the shortlist.
     if len(list_names) > num_names:
         shortlist = random_name_generator(list_names, num_names)
 
