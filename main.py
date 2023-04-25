9f8a6d823873dac0dda7d96d736cf98ab994fd4d
HineshX <hinesh.tailor@xandertalent.com>
2023-04-25 11:05:54 +0100

    made some changes to code

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import random
 def random_name_generator():
     # Prompts user to input names and creates a list
     list_of_names = input("Enter a list of names seperated by commas:")
-    list_of_names = list_of_names.split(", ")
+    list_of_names = list_of_names.split(",")
     list_of_names = [i.strip(" ") for i in list_of_names]
 
     # Prompts user to input number of names he would like
     while True:
         num_of_names = input("Enter the number of names you would like:")
-        if num_of_names.isdigit() is False or len(list_of_names) < int(num_of_names) or int(num_of_names) <= 0:
+        if not num_of_names.isdigit() or len(list_of_names) < int(num_of_names) or int(num_of_names) <= 0:
             print("Please enter an integer which is greater than 0 and less than or equal to the number number of names you have added")
             continue
         else:
@@ -21,5 +21,6 @@ def random_name_generator():
     print("Here are your names:")
     for name in selected_names:
         print(name)
+    return random_name_generator()
 
 random_name_generator()
