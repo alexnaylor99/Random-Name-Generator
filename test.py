1154c364b5b5853401f01dee4d96fb4e35d879ef
Anson Chow <anson.chow@xandertalent.com>
2023-04-25 04:02:42 +0100

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-"""name = input("Enter random names with a space: ").split(' ')"""
-name = ['ad', 'da w', 'dawwa']
-name_list = []
-for n in name:
-    if n.isalpha():
-        name_list.append(n)
-print(name_list)
