9330e5b65f6bdfe217ba61607e00ac31d8d02b2c
Anson Chow <anson.chow@xandertalent.com>
2023-04-25 04:00:50 +0100

    Update everything

--- main.py
+++ main.py
@@ -1,34 +1,49 @@
 import random
 
 def input_name():
-    name = input("Please enter alpha names with a space in between: ").split(' ')
-    name_list = []
-    for n in name:
-        if n.isalpha():
-            print(f"{n} is now added into the database, thanks for entering the name! ")
-            name_list.append(n)
-    return name_list
+    """
+    Asks the user to input a list of names, validates them, removes duplicates and returns a list of valid names.
+    """
+    # Ask the user to input a string of names separated by spaces
+    name_input = input("Please enter alpha names separated by spaces: ")
+    name_list = list(set(name_input.split()))  # Split and remove duplicates
+    name_list = [name.title() for name in name_list if name.isalpha()]  # Validate names
+
+    # Check if at least one valid name was entered
+    if not name_list:
+        print("Please enter at least one valid name.")
+        return input_name()
+    else:
+        print(f"The following {len(name_list)} names were added to the database: {', '.join(name_list)}")
+        return name_list
 
 def input_length(name_list):
-    name_times = input(f"How many names do you want randomly selected from the database? Min: 1, Max: {len(name_list)}\nanswer: ")
-    if not name_times.isnumeric() or int(name_times) == 0:
-        print("Please enter a valid number that is greater than 0.  ")
-        input_length(name_list)
-    elif int(name_times) > len(name_list):
-        print(f"Please enter a number that equal or lower than {len(name_list)}. ")
-        input_length(name_list)
-    else:
-        print("Thank you, the app will generate random names now. ")
-        return int(name_times)
+    """
+    Asks the user for the number of names to select randomly from the list.
+    """
+    # Ask the user to input a number of names to select randomly
+    while True:
+        try:
+            name_num = int(input(f"How many names do you want randomly selected from the database? Min: 1, Max: {len(name_list)}\nanswer: "))
+            if 1 <= name_num <= len(name_list):
+                return name_num
+            else:
+                print(f"Please enter a number between 1 and {len(name_list)}")
+        except ValueError:
+            print("Please enter a valid number.")
 
-def print_names(name_list, name_times):
-    if len(name_list) == 1:
-        print("Only one name is selected")
-    else:
-        random_names = random.sample(name_list, name_times)
-        print(f"Random names are {random_names}, thanks for using the app. ")
+def print_names(name_list, name_num):
+    # Select a random sample of names from the name_list and print them
+    random_names = random.sample(name_list, name_num)
+    print("Random names: ")
+    for n in random_names:
+        print (n)
+    print("Thanks for using the app! ")
 
 if __name__ == "__main__":
+    # Get a list of unique names from the user
     name_list = input_name()
-    name_times = input_length(name_list)
-    print_names(name_list, name_times)
+    # Get the number of names to select randomly from the user
+    name_num = input_length(name_list)
+    # Print the randomly selected names
+    print_names(name_list, name_num)
