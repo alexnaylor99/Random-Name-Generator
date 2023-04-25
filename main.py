6e5f6c5ac00ef055d8fae8e8f1ed443a026df73a
Ruy Zambrano <Ruyzambrano@gmail.com>
2023-04-25 11:48:46 +0100

    Added PEP8 styling

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import random
+'''
+The `random` module provides functions for generating random
+ numbers and making random selections in Python.
+'''
 
-def random_name_generator(list_names, num_names):
+
+def random_name_generator(shortlist_names, int_names):
     '''
     Takes in a list of names and an amount of names desired, 
     then uses the random module to return a list of names.
-    
+
     Parameters
     ----------
     list_names : List of strings
@@ -14,17 +19,17 @@ def random_name_generator(list_names, num_names):
     -------
     Function returns a list of names
     '''
-    shortlist = random.sample(list_names, num_names)
-    return shortlist
-
+    random_names = random.sample(shortlist_names, int_names)
+    return random_names
 
 
 if __name__ == '__main__':
-    
+
     # Prompt the user to input a list of names and split them into a list.
     while True:
         names = input('Enter a list of names that you are '
-                  'choosing from, separated by a space (eg: "Anna Elsa Moana"): ')
+                      'choosing from, separated by a space '
+                      '(eg: "Anna Elsa Moana"): ')
         
         # Check if the user has input names.
         if names == '':
@@ -34,24 +39,23 @@ if __name__ == '__main__':
         list_names = names.split()
         break
 
-
     while True:
         # Prompt the user to input an integer and validate.
         try:
-            num_names = int(input(f'You have {len(list_names)} names in your list. '
-                                  'How many names do you want?\n'))
+            num_names = int(input(f'You have {len(list_names)} names in your '
+                                  'list. How many names do you want?\n'))
             if num_names <= 0:
                 print('You must choose a number greater than 0. Try again')
                 continue
             break
         except ValueError as e:
             print(f'Error: {e}.\nYou must enter an integer. Try again')
-    
+
     # Generate the shortlist. 
     if len(list_names) > num_names:
         shortlist = random_name_generator(list_names, num_names)
 
-    # Return the whole list if the number of names is less than the number required.
+    # Return entire list of names if there aren't enough.
     else:
         shortlist = list_names
 
